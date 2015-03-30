e0f02e1dcef3a91dbfb890b7274ab657dabbaf41
Damon McMinn <damon@mccarthyscott.com>
2015-03-30 12:28:53 +0100

    Catch errors caused by null match

--- proxy.js
+++ proxy.js
@@ -8,11 +8,18 @@ var proxy = httpProxy.createProxyServer({});
 http.createServer(function(req, res) {
 
   var target;
+  var api;
   var identifier = /^\/.+(?=\/)/i;
-  var api = req.url.match(identifier)[0].replace('/', '').toLowerCase();
-  req.url = req.url.replace(identifier, '');
+
+  try {
+    api = req.url.match(identifier)[0].replace('/', '').toLowerCase();
+    req.url = req.url.replace(identifier, '');
+  } catch (e) {
+    api = 'error';
+  }
 
   proxy.web(req, res, {
     target: services[api] ? services[api] : services.error
   });
+
 }).listen(8080);
