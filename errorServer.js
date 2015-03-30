39beb763423b09836cbd9adba8e6171837b1fb1e
Damon McMinn <damon@mccarthyscott.com>
2015-03-30 13:19:28 +0100

    Error code should reflect there is an error!

--- errorServer.js
+++ errorServer.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 module.exports = http.createServer(function(req, res) {
-  var message = JSON.stringify({message: 'Service not found'});
-  res.writeHead(200, {'Content-Type': 'application/json'});
+  var message = JSON.stringify({message: 'Service does not exist'});
+  res.writeHead(400, {'Content-Type': 'application/json'});
   res.write(message);
   res.end();
 }).listen(8081);
